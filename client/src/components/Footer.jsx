70bbd83b949fb72fcfcd5a2d562151315c3223b7
Anton Bangoura <bangouranton@gmail.com>
2022-07-13 14:02:37 +0200

    init

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => (
     <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
     <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-      <p className="text-white text-left text-xs">@kryptomastery2022</p>
+      <p className="text-white text-left text-xs">@Anton Bangoura</p>
       <p className="text-white text-right text-xs">All rights reserved</p>
     </div>
   </div>
